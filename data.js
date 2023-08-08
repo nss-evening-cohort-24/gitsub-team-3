1fc74942a53f3c199897de92ab0d88f232f7a19a
Gregory Markus <gregmarkus1992@yahoo.com>
2023-08-07 21:56:41 -0500

    created dummy data for rendering: repos, projects, and packages. also added an images folder

---
+++ data.js
@@ -0,0 +1,142 @@
+const repos = [
+{
+  id: 1,
+  name: "zoom-updates-randomly",
+  description:"why does it do this all the time randomly constantly?!",
+  tags: [
+    "random",
+    "js",
+    "react"
+  ],
+  fave: true,
+  activity: url="images/githubActivity.png",
+  codebase: "js",
+  faveNum: 63,
+  branchNum: 42,
+  updatedLast: "Oct 23rd, 2023"
+},
+{
+  id: 2,
+  name: "improv-goldfish-joke-maker",
+  description:"you can lead a fish to water but is it gonna know how to make a joke?",
+  tags: [
+    "jokes",
+    "react",
+    "joke.nss"
+  ],
+  fave: false,
+  activity: url="images/GHactivity.png",
+  codebase: "c++",
+  faveNum: 64,
+  branchNum: 2,
+  updatedLast: "Sep 21, 2018"
+},
+{
+  id: 3,
+  name: "balloon-popper",
+  description:"POP!",
+  tags: [
+    "fun",
+    "C",
+    "no more balloons"
+  ],
+  fave: true,
+  activity: url="images/githubActivity.png",
+  codebase: "C",
+  faveNum: 22,
+  branchNum: 94,
+  updatedLast: "Aug 3rd, 1908"
+},
+{
+  id: 4,
+  name: "glasses-polisher",
+  description:"it does it for you!",
+  tags: [
+    "cleaner",
+    "wireframe",
+    "glare"
+  ],
+  fave: true,
+  activity: url="images/githubActivity.png",
+  codebase: "Java",
+  faveNum: 5000,
+  branchNum: 9000,
+  updatedLast: "Nov 14th, 1984"
+},
+]
+
+const projects = [
+  {
+    id: 1,
+    open: true,
+    name: "codeBtr",
+    updatedLast: "June 30th, 2020",
+    description: "This makes you code better dummy"
+  },
+  {
+    id: 2,
+    open: false,
+    name: "jobGtr",
+    updatedLast: "Feb 9th, 2019",
+    description: "Gets you a job"
+  },
+  {
+    id: 3,
+    open: false,
+    name: "gitGood",
+    updatedLast: "Apr 8th, 1992",
+    description: "Gets you gooder"
+  },
+  {
+    id: 4,
+    open: false,
+    name: "style-ish",
+    updatedLast: "Mar 3rd, 1801",
+    description: "Makes you dress better"
+  },
+  {
+    id: 5,
+    open: false,
+    name: "forgetIt",
+    updatedLast: "Feb 9th, 2014",
+    description: "Huh?"
+  },
+  {
+    id: 6,
+    open: false,
+    name: "bananaPlr",
+    updatedLast: "Feb 30th, 2000",
+    description: "Peels bananas"
+  },
+]
+
+const packages = [
+  {
+    id:1,
+    icon: url="images-g/Soft_drink_icon.png",
+    name: "mountainDo" ,
+    description:"does the dew" ,
+    tags:["beta", "java"]
+  },
+  {
+    id:2,
+    icon: url="images-g/water-drop1.png",
+    name: "evianescents",
+    description:"wake me up wake me up inside" ,
+    tags:["react", "C", "C++"]
+  },
+  {
+    id:3,
+    icon: url="images-g/Sabathius-Fire-warning-symbol.png",
+    name:"formulaFire" ,
+    description: "files everything in proper order",
+    tags:["tailwind","EZ", "search-bar"]
+  },
+  {
+    id:4,
+    icon: url="images-g/man_disguise_glasses.png",
+    name: "GR8googly",
+    description: "googles things you wonder about while your walking in the mall alone with your headphones in by itself and saves all the answers in a note document for later",
+    tags:["theOne","bigGuyCenter","note-taker"]
+  },
+]
