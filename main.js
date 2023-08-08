2c44e1667c46aaa45757797fc24d21514f3dbe5e
Gregory Markus <gregmarkus1992@yahoo.com>
2023-08-08 18:54:22 -0500

    got the package on dom function working changed a couple things in data to be more accurate

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("Connected")
+import { renderToDom } from "./utility/utility.js";
+import { packages } from "./data.js";
+const packageOnDom = (array) => {
+  let domString = "";
+  for (const pack of array) {
+    domString += `
+  <div class="card" style="width: 18rem;">
+    <img src="${pack.icon}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${pack.name}</h5>
+      <p class="card-text">${pack.description}</p>
+      <a href="#" class="btn btn-primary">Learn more...</a>
+    </div>
+  </div>
+`;
+  }
+  renderToDom("#package-container", domString);
+};
+packageOnDom(packages)
