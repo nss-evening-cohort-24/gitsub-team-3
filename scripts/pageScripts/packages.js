b94a3f6a4ff8c4029e76ea5581652985aac9bb7f
Gregory Markus <gregmarkus1992@yahoo.com>
2023-08-09 20:40:47 -0500

    got the submit to work :)

--- scripts/pageScripts/packages.js
+++ scripts/pageScripts/packages.js
@@ -6,7 +6,7 @@ export const packageOnDom = (array) => {
   let domString = "";
   for (const pack of array) {
     domString += `
-  <div class="card" style="width: 18rem;">
+  <div id="package-card" class="card" style="width: 18rem;">
     <img src="${pack.icon}" class="card-img-top" alt="...">
     <div class="card-body">
       <h5 class="card-title">${pack.name}</h5>
@@ -18,23 +18,55 @@ export const packageOnDom = (array) => {
   }
   renderToDom("#package-container", domString);
 };
-
+packageOnDom(packages)
 
 export const packageFormOnDom = () => {
   let domString = "";
-  domString += `<div class="mb-3">
-  <label for="exampleFormControlInput1" class="form-label">Create new package</label>
-  <input type="text" class="form-control" id="package-form-name" placeholder="name">
-</div>
-<div class="mb-3">
-  <label for="package-form-description" class="form-label">Description</label>
-  <textarea class="form-control" id="package-form-description" rows="3"></textarea>
-</div>
-<input class="btn btn-primary" type="submit" value="Submit">
-`;
-renderToDom("#package-form",domString)
-};
+  domString += `
+  <form id="package-form">
 
-profileOnDom(profile)
-packageOnDom(packages);
-packageFormOnDom();
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Package name</label>
+      <input type="text" class="form-control" id="package-name-input" placeholder="Enter name here">
+    </div>
+
+    <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+      <textarea class="form-control" id="package-description-input" placeholder="Write a short description here" rows="3"></textarea>
+    </div>
+
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Icon</label>
+      <input type="url" class="form-control" id="package-icon-input" placeholder="Image url">
+    </div>
+   
+    <input class="form-control form-control-sm" type="text" placeholder="Tags" aria-label=".form-control-sm example">
+
+    <div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </div>
+
+  </form>
+`;
+  renderToDom("#package-form-container", domString);
+};
+packageFormOnDom()
+
+const packageForm = document.getElementById("package-form")
+
+packageForm.addEventListener('submit',(e) => {
+  e.preventDefault()  
+
+    const newPackage = {
+      id: packages.length +1,
+      icon: document.getElementById('package-icon-input').value,
+      name: document.getElementById('package-name-input').value,
+      description: document.getElementById('package-description-input').value,
+    };
+
+    packages.push(newPackage)
+    packageOnDom(packages)
+    packageForm.reset()
+  }
+  )
+ profileOnDom(profile)
