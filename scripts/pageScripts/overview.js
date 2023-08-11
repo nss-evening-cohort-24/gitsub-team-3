09d9d1ffcd70f70a0b2a0720579bd3a4b873a8d8
Keana Cobarde <keanacobarde@gmail.com>
2023-08-10 20:50:22 -0500

    reformatting with prettier

--- scripts/pageScripts/overview.js
+++ scripts/pageScripts/overview.js
@@ -3,21 +3,23 @@ import { renderToDom } from "../../utility/renderToDom.js";
 import { profileOnDom } from "../main.js";
 import { repos } from "../../data/data.js";
 
-profileOnDom(profile)
+profileOnDom(profile);
 
 // *********  OVERVIEW - Cards ********** //
 const renderPinnedCards = () => {
-  let domString = ``; 
-repos.filter((repo) => repo.fave).forEach((repo) => {
-  domString += `<div class="card" style="width: 18rem; border-radius:%; border-color:grey; margin:0.5em">
+  let domString = ``;
+  repos
+    .filter((repo) => repo.fave)
+    .forEach((repo) => {
+      domString += `<div class="card" style="width: 18rem; border-radius:%; border-color:grey; margin:0.5em">
   <div class="card-body">
     <h5 class="card-title"><span class="material-symbols-outlined">book</span> ${repo.name} </h5>
     <p class="card-text">${repo.description}</p>
   </div>
-</div>`
-})
-renderToDom("#pinned-repos", domString); 
-}
+</div>`;
+    });
+  renderToDom("#pinned-repos", domString);
+};
 renderPinnedCards();
 
 // *********  OVERVIEW - FORM ********** //
@@ -34,25 +36,25 @@ const pinnedRepoFormHTML = () => {
   <textarea class="form-control" id="pinnedRepoDesc" rows="4" style="background-color:black;border-color:#6c6e72"></textarea>
   <hr>
   <button type="submit" class="btn btn-success">Pin It!</button>
-  </div></form>`
+  </div></form>`;
   renderToDom("#pinned-repo-form-container", domString);
-}
-pinnedRepoFormHTML(); 
+};
+pinnedRepoFormHTML();
 
 // *********  OVERVIEW - Event Listeners ********** //
 const eventListeners = () => {
-//Form Submission Event Listener
-  document.querySelector("#overview-form").addEventListener('submit', (e) => {
-    e.preventDefault(); 
-   const newFave = {
-    id: repos.length + 1,
-    name: document.querySelector("#pinnedRepo").value,
-    description: document.querySelector("#pinnedRepoDesc").value,
-    fave: true,
-   }
-   repos.push(newFave);
-   renderPinnedCards();
-   document.querySelector("#overview-form").reset(); 
-  })
-}
-eventListeners(); 
+  //Form Submission Event Listener
+  document.querySelector("#overview-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newFave = {
+      id: repos.length + 1,
+      name: document.querySelector("#pinnedRepo").value,
+      description: document.querySelector("#pinnedRepoDesc").value,
+      fave: true,
+    };
+    repos.push(newFave);
+    renderPinnedCards();
+    document.querySelector("#overview-form").reset();
+  });
+};
+eventListeners();
