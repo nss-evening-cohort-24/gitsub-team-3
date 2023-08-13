2b4605f04d6b500471075f621e4059ad7c03b51a
Gregory Markus <gregmarkus1992@yahoo.com>
2023-08-13 18:08:03 -0500

    added delete button and functionality changed postion of package cards

--- scripts/pageScripts/packages.js
+++ scripts/pageScripts/packages.js
@@ -11,13 +11,13 @@ export const packageOnDom = (array) => {
       <h5 class="card-title"><img src="${pack.icon}" class="card-img-top" alt="...">${pack.name}</h5>
       <p class="card-text">${pack.description}</p>
       <button type="button" class="btn btn-secondary btn-sm">Learn more...</button>
+      <button type="button" class="btn btn-danger" id="delete-btn--${pack.id}">Delete</button>
     </div>
   </div>
 `;
   }
   renderToDom("#package-container", domString);
 };
-
 
 export const packageFormOnDom = () => {
   let domString = "";
@@ -26,17 +26,17 @@ export const packageFormOnDom = () => {
     <h1>Create new package</h1>
     <div class="mb-3">
       <label for="exampleFormControlInput1" class="form-label">Package name</label>
-      <input type="text" class="form-control" id="package-name-input" placeholder="Enter name here">
+      <input type="text" class="form-control" id="package-name-input" placeholder="Enter name here" required>
     </div>
 
     <div class="mb-3">
       <label for="exampleFormControlTextarea1" class="form-label">Description</label>
-      <textarea class="form-control" id="package-description-input" placeholder="Write a short description here" rows="3"></textarea>
+      <textarea class="form-control" id="package-description-input" placeholder="Write a short description here" rows="3" required></textarea>
     </div>
 
     <div class="mb-3">
       <label for="exampleFormControlInput1" class="form-label">Icon</label>
-      <input type="url" class="form-control" id="package-icon-input" placeholder="Image url">
+      <input type="url" class="form-control" id="package-icon-input" placeholder="Image url" required>
     </div>
 
     <div>
@@ -48,24 +48,38 @@ export const packageFormOnDom = () => {
   renderToDom("#package-form-container", domString);
 };
 
+const eventListeners = () => {
+    const packageForm = document.getElementById("package-form");
 
-const packageForm = document.getElementById("package-form-container")
+    packageForm.addEventListener("submit", (e) => {
+      e.preventDefault();
 
-packageForm.addEventListener('submit',(e) => {
-  e.preventDefault()  
+      const newPackage = {
+        id: packages.length + 1,
+        icon: document.getElementById("package-icon-input").value,
+        name: document.getElementById("package-name-input").value,
+        description: document.getElementById("package-description-input").value,
+      };
 
-    const newPackage = {
-      id: packages.length +1,
-      icon: document.getElementById('package-icon-input').value,
-      name: document.getElementById('package-name-input').value,
-      description: document.getElementById('package-description-input').value,
-    };
+      packages.push(newPackage);
+      packageOnDom(packages);
+      packageForm.reset();
+    });
 
-    packages.push(newPackage)
-    packageOnDom(packages)
-    packageForm.reset()
-  })
+    document
+      .querySelector("#package-container")
+      .addEventListener("click", (event) => {
+        if (event.target.id.includes("delete")) {
+          const [, int] = event.target.id.split("--");
 
+          const index = packages.findIndex((pack) => pack.id === Number(int));
+
+          packages.splice(index, 1);
+
+          packageOnDom(packages);
+        }
+      });
+  };
 
 const startPackages = () => {
   packageOnDom(packages)
@@ -73,5 +87,7 @@ const startPackages = () => {
   navbarOnDom()
   profileOnDom(profile)
   footerOnDom()
+  eventListeners()
 }
+
 startPackages()
